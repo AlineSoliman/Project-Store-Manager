5b33da9da147777a127e66510dde9f208ce66d6e
Aline Soliman <alineapreis@hotmail.com>
2022-08-16 17:48:52 -0300

    feat: cria função getId na pasta productsService.js

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,5 +1,11 @@
+const CustomError = require('../errors/customError');
 const productsModel = require('../models/productsModel');
 
 const getAll = async () => productsModel.getAll(); // regra de negócio
 
-module.exports = { getAll };
+const getId = async (id) => {
+  const product = await productsModel.getId(id);
+  if (!product) throw new CustomError(404, 'not found', 'Product not found');
+  return product;
+};
+module.exports = { getAll, getId };
