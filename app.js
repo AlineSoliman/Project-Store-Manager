39dd3c1370d20c75d07340850cb593356d2b78a4
Aline Soliman <alineapreis@hotmail.com>
2022-08-16 17:38:04 -0300

    build: instala biblioteca express-async-errors e chama rota para tratar erros

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+require('express-async-errors');
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
+
+const productsRoute = require('./routes/productsRoutes');
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use('/products', productsRoute);
+
+app.use(errorMiddleware);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
