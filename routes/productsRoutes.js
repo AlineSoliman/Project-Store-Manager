0eac27779e2e490f32350e0f2eddbbcaedb07713
Aline Soliman <alineapreis@hotmail.com>
2022-08-16 17:50:36 -0300

    feat: insere rota para /product no arquivo productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,5 +5,6 @@ const productsController = require('../controllers/productController');
 const productsRoute = express.Router();
 
 productsRoute.get('/', productsController.getAll);
+productsRoute.get('/:id', productsController.getId);
 
 module.exports = productsRoute;
