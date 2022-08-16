c2a2da353f1fe3cb89c7041c309b1075ec38d167
Aline Soliman <alineapreis@hotmail.com>
2022-08-16 17:47:52 -0300

    feat: cria função getId na pasta productsModel.js

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,8 +1,13 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const result = await connection.execute('SELECT * FROM StoreManager.products');
+  const [result] = await connection.execute('SELECT * FROM StoreManager.products');
   return result;
 };
 
-module.exports = { getAll };
+const getId = async (id) => {
+  const [[result]] = await connection
+    .execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
+  return result;
+};
+module.exports = { getAll, getId };
