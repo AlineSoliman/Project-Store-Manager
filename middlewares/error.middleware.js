f9e9d1d4dc727b2cf19cca51d2a18efd940bc6d3
Aline Soliman <alineapreis@hotmail.com>
2022-08-16 17:46:46 -0300

    feat: cria arquivo com middleware de erro

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+const errorMiddleware = (err, _req, res, _next) => {
+  const erro = {
+    error: {
+      code: err.code || 'underfinedError',
+      message: err.message,
+    },
+  };
+  res.status(err.status || 500).json(erro.error);
+};
+
+module.exports = errorMiddleware;
