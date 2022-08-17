11570256a0e7db306929ab88108092ad6811a586
Aline Soliman <alineapreis@hotmail.com>
2022-08-17 18:06:14 -0300

    feat: cria função saveServiceProduct na camanda productServices para inserção de novo produto no banco de dados

--- services/productsServices.js
+++ services/productsServices.js
@@ -3,9 +3,13 @@ const productsModel = require('../models/productsModel');
 
 const getAll = async () => productsModel.getAll(); // regra de negócio
 
+const saveServiceProduct = async (product) => {
+  const savedProducts = await productsModel.saveModelProduct(product);
+  return savedProducts;
+};
 const getId = async (id) => {
   const product = await productsModel.getId(id);
   if (!product) throw new CustomError(404, 'not found', 'Product not found');
   return product;
 };
-module.exports = { getAll, getId };
+module.exports = { getAll, getId, saveServiceProduct };
