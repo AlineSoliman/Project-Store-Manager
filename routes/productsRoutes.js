41e658f2614be105e754f679363992888d950b60
Aline Soliman <alineapreis@hotmail.com>
2022-08-17 18:07:15 -0300

    feat: cria rota post no arquivo productsRoutes.js para inserção de novo produto no banco de dados

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,7 +4,10 @@ const productsController = require('../controllers/productController');
 
 const productsRoute = express.Router();
 
+const productValidate = require('../middlewares/validateProduct');
+
 productsRoute.get('/', productsController.getAll);
 productsRoute.get('/:id', productsController.getId);
+productsRoute.post('/', productValidate.productMiddleware, productsController.saveProduct);
 
 module.exports = productsRoute;
