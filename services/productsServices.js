e16cabead6d68ee59747ac3027b19fbfea6c7a21
Aline Soliman <alineapreis@hotmail.com>
2022-08-18 19:38:05 -0300

    fix: ajusta funções que não subiram ao reverter commit com nome errado - insere funções na camada services para cadastro de vendas no banco de dados

--- services/productsServices.js
+++ services/productsServices.js
@@ -12,4 +12,25 @@ const getId = async (id) => {
   if (!product) throw new CustomError(404, 'not found', 'Product not found');
   return product;
 };
-module.exports = { getAll, getId, saveServiceProduct };
+
+const salesProducts = async (sold) => {
+  const validateId = sold.map(({ productId }) => productsModel.getId(+productId));
+  const validProductId = await Promise.all(validateId);
+  const soldProduct = validProductId.some((productId) => !productId);
+  if (soldProduct) throw new CustomError(404, 'not found', 'Product not found');
+
+  const sales = await productsModel.salesTable();
+
+  const { insertId } = sales;
+
+  const infoProductsSales = sold.map((product) => productsModel.salesProducts(
+    +insertId, +product.productId, +product.quantity,
+  ));
+  await Promise.all(infoProductsSales);
+  return {
+    id: insertId,
+    itemsSold: sold,
+  };
+};
+
+module.exports = { getAll, getId, saveServiceProduct, salesProducts };
