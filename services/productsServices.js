6fdc7d2c5eda585029d1e077990c6c0aacfdb6b8
Aline Soliman <alineapreis@hotmail.com>
2022-08-18 19:28:25 -0300

    feat: cria funções na camada Service para inserir dados de vendas

--- services/productsServices.js
+++ services/productsServices.js
@@ -12,25 +12,4 @@ const getId = async (id) => {
   if (!product) throw new CustomError(404, 'not found', 'Product not found');
   return product;
 };
-
-const salesProducts = async (sold) => {
-  const validateId = sold.map(({ productId }) => productsModel.getId(+productId));
-  const validProductId = await Promise.all(validateId);
-  const soldProduct = validProductId.some((productId) => !productId);
-  if (soldProduct) throw new CustomError(404, 'not found', 'Product not found');
-
-  const sales = await productsModel.salesTable();
-
-  const { insertId } = sales;
-
-  const infoProductsSales = sold.map((product) => productsModel.salesProducts(
-    +insertId, +product.productId, +product.quantity,
-  ));
-  await Promise.all(infoProductsSales);
-  return {
-    id: insertId,
-    itemsSold: sold,
-  };
-};
-
-module.exports = { getAll, getId, saveServiceProduct, salesProducts };
+module.exports = { getAll, getId, saveServiceProduct };
