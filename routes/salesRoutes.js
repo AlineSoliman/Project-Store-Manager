e828a0c8d71354447d15681b00d46ed4de948f97
Aline Soliman <alineapreis@hotmail.com>
2022-08-19 17:49:22 -0300

    feat: cria rota para endpoint que lista as vendas de modo geral e específico

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -6,6 +6,8 @@ const salesRoute = express.Router();
 
 const salesValidate = require('../middlewares/validateSoldProduct');
 
+salesRoute.get('/', productsController.getAllSales);
+salesRoute.get('/:id', productsController.getSalesId);
 salesRoute.post('/', salesValidate.salesMiddleware, productsController.salesProducts);
 
 module.exports = salesRoute;
